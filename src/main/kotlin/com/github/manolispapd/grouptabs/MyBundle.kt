c5bca9c449d9ec559d0650743ccd633637d4f6fc
GitHub Action <action@github.com>
2022-10-08 14:47:00 +0000

    Template cleanup

--- src/main/kotlin/com/github/manolispapd/grouptabs/MyBundle.kt
+++ src/main/kotlin/com/github/manolispapd/grouptabs/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.manolispapd.grouptabs
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/manolispapd/grouptabs/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/manolispapd/grouptabs/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.manolispapd.grouptabs.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.manolispapd.grouptabs.MyBundle
 
 class MyApplicationService {
 
